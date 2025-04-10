d9419cda4a55ecb8e5086385ffd7a1951a80a7ac
Ubuntu <ubuntu@localhost.localdomain>
2025-04-10 18:49:13 +0800

    阅文参数

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@ AE=/home/ubuntu/models/flux.1-dev/ae.safetensors \
     FLUX_DEV=/home/ubuntu/models/flux.1-dev/flux1-dev.safetensors \
     T5=/home/ubuntu/models/xlabs-ai-xflux_text_encoders \
     CLIP=/home/ubuntu/models/openai-clip-vit-large-patch14 \
-    python3 ./main.py --width=256 --height=256 --seed=10 --model_type=flux-dev --num_steps=30 --prompt='a dog' --device=cuda --num_images_per_prompt=2 --custom_offload=True
+    python3 ./main.py --width=816 --height=1449 --seed=10 --model_type=flux-dev --num_steps=25 \
+    --prompt='1man, adult male, exquisitely beautiful facial features, the highest image quality, close - up portrait, exquisite painting style, delicate face, waist-up pose in standing position: 1.1, occupying 50% of the frame, look at camera, upper body, full clothes, normal fingers,' \
+    --device=cuda --num_images_per_prompt=2 --custom_offload=True --guidance=8 --timestep_to_start_cfg=999
